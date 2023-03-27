87e3eaab63cb555b43189728b2cdbeb485d391c6
Henrique2019 <48299527+Henrique2019@users.noreply.github.com>
2023-03-26 23:23:49 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -122,7 +122,7 @@ export default Home
 export const getStaticProps: GetStaticProps = async () => {
   const { data } = await api.get('episodes', {
     params: {
-      _limit: 12,
+      _limit: 20,
       _sort: 'published_at',
       _order: 'desc',
     },
